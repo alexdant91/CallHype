1ec6506708e0d1ba946114c6e83ef7cca7569b37
Alessandro D'Antoni <alexdant91@gmail.com>
2019-08-10 10:31:45 +0200

    Animation and main.js improvements

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,30 @@
 $(document).ready(function () {
+    // CONFIGURATION
+    const _url = "";
+    const _method = "POST"
 
+    // INIT
     onScreen('animateOnScreen', 'fadeInDown');
+
+    // Const elements
+    const $contactUs = $('#contact-us');
+    $contactUs.on('submit', function () {
+        const data = new FormData(this);
+        $.ajax({
+            url: _url,
+            cache: false,
+            processData: false,
+            contentType: false,
+            method: _method,
+            data: data,
+            success: function (data) {
+                // Handle the result and display the correct message
+            },
+            error: function (a, b, c) {
+                // Handle the result and display an error message
+            }
+        });
+    });
 
 });
 
@@ -24,20 +48,26 @@ const scrollSpy = ($target, offset = 0) => {
 }
 
 const onScreen = (elementsClassName, animationName) => {
+    let offset = 150;
     $(`.${elementsClassName}`).each(function () {
         const $elem = $(this);
+        let $offset = $elem.data('offset');
+        // Find the data option
+        offset = $offset != undefined ? $offset : offset;
+        animationName = $elem.data('animation') != undefined ? $elem.data('animation') : animationName;
+        // END data option
         const top = $elem.offset().top;
         const bottom = top + $elem.height();
         const pageHeight = $(window).height();
         let pageScroll = $('body').scrollTop();
         let pageScrollFull = pageScroll + pageHeight;
-        if (top > pageScroll && bottom < pageScrollFull + 150) {
+        if (top > pageScroll && bottom < pageScrollFull + offset) {
             $elem.addClass(`animated ${animationName}`);
         }
         $(document).on('scroll', function () {
             pageScroll = $(this).scrollTop();
             pageScrollFull = pageScroll + pageHeight;
-            if (top > pageScroll && bottom < pageScrollFull + 150) {
+            if (top > pageScroll && bottom < pageScrollFull + offset) {
                 if (!$elem.hasClass('animated')) {
                     $elem.addClass(`animated ${animationName}`);
                 }
